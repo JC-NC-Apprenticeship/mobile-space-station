6db645b0cb2a03acca03ff086aa99d5b92752c96
Justin Heath <just.coder@outlook.com>
2022-01-25 20:55:40 +0000

    new

--- src/stage-01/OrientationScreen.jsx
+++ src/stage-01/OrientationScreen.jsx
@@ -1,4 +1,63 @@
 import React from 'react';
-import { PlaceholderScreen } from '../_internal/components/PlaceholderScreen';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { Card, Title, Paragraph } from 'react-native-paper';
 
-export const OrientationScreen = () => <PlaceholderScreen lessonLink="introduction" />;
+import user from '../../assets/user.jpg';
+
+export const OrientationScreen = () => {
+  return (
+    <View style={styles.mainDiv}>
+      <Text style={styles.text}>Hello World!!</Text>
+      <View style={styles.mainCard}>
+        <Image
+          style={styles.tinyLogo}
+          source={user}
+        />
+        <View>
+          <Title style={styles.cardText}>Just Coder</Title>
+          <Paragraph style={styles.cardText}>Super Powers: None.....</Paragraph>
+        </View>
+      </View>
+    </View>
+    )
+
+
+};
+const styles = StyleSheet.create({
+  mainDiv: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'rgb(0,25,51)',
+  },
+  text: {
+    color: 'white',
+    fontSize: 23,
+    marginTop: 20,
+  },
+
+  mainCard: {
+    flex: 0.25,
+    flexDirection: "row",
+    width: 350,
+    justifyContent: 'space-around',
+    // alignContent: 'center',
+    alignItems: 'center',
+
+
+    backgroundColor: 'rgb(2, 62, 138)',
+    borderRadius: 15,
+    marginTop: 20,
+    borderColor: "rgb(0, 150, 199)",
+    borderWidth: 5,
+  },
+  tinyLogo: {
+    width: 100,
+    height: 100,
+    borderRadius: 15,
+    borderColor: "rgb(0, 150, 199)",
+    borderWidth: 3,
+  },
+  cardText: {
+    color: 'white',
+  }
+});
--- src/stage-02/CrewManifestScreen.jsx
+++ src/stage-02/CrewManifestScreen.jsx
@@ -1,28 +1,48 @@
 import React from 'react';
 
-// You'll likely want these components:
-//import { View, StyleSheet , FlatList, Pressable } from 'react-native';
-//import { Card } from 'react-native-paper';
+import { SafeAreaView, Text, View, StyleSheet , FlatList, Pressable } from 'react-native';
+import { Card } from 'react-native-paper';
 
 // This is the data containing the crew members
-// import { names } from '../data/crew';
+import { names } from '../data/crew';
 
 // This is our pre-built CachedImage component - feel free to take a look at the implementation
-// import { CachedImage } from '../third-party-components/CachedImage';
+import { CachedImage } from '../third-party-components/CachedImage';
 
 // Or any icon set you prefer!
 //import { AntDesign } from '@expo/vector-icons';
 
-// You can remove this once you've got started
-import { PlaceholderScreen } from '../_internal/components/PlaceholderScreen';
+const Item = ({ name }) => (
+  <View>
+    <Text style={styles.text}>{name}</Text>
+  </View>
+);
+
 
 export const CrewManifestScreen = () => {
+  const renderItem = ({ item }) => (
+    <Item name={item.name} />
+  );
   return (
-    <PlaceholderScreen
-      lessonLink="mobile-for-web-developers"
-      sourceFile="src/stage-02/CrewManifestScreen.jsx "
-      demoSourceFile="src/completed-app/crew-manifest/CrewManifest.jsx"
-      demoRoute="DemoAppCrewManifest"
-    />
+    <SafeAreaView style={styles.mainDiv}>
+      <FlatList
+        data={names}
+        renderItem={renderItem}
+        keyExtractor={item => item.name}
+      />
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  mainDiv: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'rgb(0,25,51)',
+  },
+  text: {
+    color: 'red',
+    fontSize: 23,
+    marginTop: 20,
+  },  
+});
